092988aadead993eec8cf58a29ce03109ce5d2c3
Ferran Paredes <fparedlo@gmail.com>
2025-10-12 20:43:27 +0100

    Adding Vitest and first unit tests

---
+++ fakecommerce/src/utils/previousPrice.test.js
@@ -0,0 +1,6 @@
+import { expect, test } from 'vitest'
+import previousPrice from './previousPrice'
+
+test('And item that cost 100 and was discounted by 50 used to cost 200', () => {
+  expect(previousPrice(100, 50)).toBe(200)
+})
