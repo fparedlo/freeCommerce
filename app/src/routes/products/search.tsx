ff47dd59db07d884ef52504676e487c317301d24
Ferran Paredes <fparedlo@gmail.com>
2025-10-24 20:04:21 +0100

    more ui improvements for search page

--- app/src/routes/products/search.tsx
+++ app/src/routes/products/search.tsx
@@ -31,21 +31,31 @@ function RouteComponent() {
   return (
     <>
       <ProductSearch />
-      <h1 className="font-extrabold text-2xl mt-4 mb-8">Search results for: <span className="font-light">{search.q}</span></h1>
       {isPending && <Spinner />}
       {error && <ErrorInfo message={error.message} />}
-      <ul className="flex flex-wrap gap-10 w-full group is-plp">
-        {data?.map((p) => (
-          <ProductCard product={p} key={p.id} />
-        ))}
-      </ul>
-      {data?.length === 0 && (
-        <p className="text-center text-lg">
+      {data?.length === 0 ? (
+        <p className="text-center text-2xl mt-4 font-light">
           <span className="font-bold">Not products found for this search:</span>{" "}
           {search.q}
         </p>
+      ) : (
+        <>
+          <h1 className="font-extrabold text-2xl mt-4 mb-8">
+            Search results for: <span className="font-light">{search.q}</span>
+          </h1>
+          <ul className="flex flex-wrap gap-10 w-full group is-plp">
+            {data?.map((p) => (
+              <ProductCard product={p} key={p.id} />
+            ))}
+          </ul>
+        </>
       )}
-      <LinkButton url="/products/all" text="Show me all" extraClasses="mt-20" />
+
+      <LinkButton
+        url="/products/all"
+        text="Show all products"
+        extraClasses="mt-16"
+      />
     </>
   );
 }
